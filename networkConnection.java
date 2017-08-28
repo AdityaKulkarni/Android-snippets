a433abb3316d52d31901f4a60def8db7d87a34f4
Aditya Kulkarni <dangocharlie@gmail.com>
2017-08-28 18:49:04 +0530

    Update and rename Network connection.java to networkConnection.java

--- networkConnection.java
+++ networkConnection.java
@@ -1,11 +1,13 @@
 /*checks if device is connected to network*/
-if (!isNetworkAvailable()) {
-    Snackbar snackbar = Snackbar.make(linearLayout, getString(R.string.no_connection_detected), Snackbar.LENGTH_LONG);
-    snackbar.show();
-}
+
 private boolean isNetworkAvailable() {
     ConnectivityManager connectivityManager
         = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
     NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
     return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+}
+
+if (!isNetworkAvailable()) {
+    Snackbar snackbar = Snackbar.make(linearLayout, getString(R.string.no_connection_detected), Snackbar.LENGTH_LONG);
+    snackbar.show();
 }
